28fbfc0d0f7cb6d04749dcf7f82fb5c9e7d847d5
AliAhmad <117520357+A-Ahmad-A@users.noreply.github.com>
2023-08-12 01:52:50 +0500

    Footer Review and other
    Footer revew changes, email addition and certifications additions

--- src/pages/components/Footer.jsx
+++ src/pages/components/Footer.jsx
@@ -5,6 +5,7 @@ import "@fontsource/poppins";
 import "@fontsource/poppins/400.css";
 import goals from "../images/goals.png";
 import safe from "../images/safecontractor.png";
+import ico from "../images/ico.png";
 
 import { Link } from "react-router-dom";
 
@@ -27,31 +28,74 @@ const Footer = () => {
           <Link to="/contact" className="flinks">
             <h1 className="footerL">Contact us</h1>
           </Link>
-          
         </div>
 
         <div className="dispayColumn">
-        <h1 className="footerHeading">Address</h1>
+          <h1 className="footerHeading">Address</h1>
           <p>Coventry city centre, CV1</p>
-          
+
           <h1 className="footerHeading">Contact us</h1>
           <p>
-            Email :{" "}
             <span className="blueText">info@spyconsecurity.co.uk </span>{" "}
           </p>
-        
+          <p>
+            <span className="blueText">complaints@spyconsecurity.co.uk </span>{" "}
+          </p>
         </div>
 
-        <div style={{display:"flex", flexDirection:"column"}}>
-        <h1 className="footerHeading">Certifications</h1>
-          <img src={goals} className="footerLogoservices" />
-          <img src={safe} style={{marginTop:"10px"}}className="footerLogoservices" />
-        </div>
-      </div>    
-      <div className="bottomLine">
-      <Link to="/" className="flinks">
-      <img src={logo} className="footerLogo" />
+        <div style={{ display: "flex", flexDirection: "column" }}>
+          <h1 className="footerHeading">Services</h1>
+          <Link to="/services" className="flinks">
+            <h1 className="footerL">Fire alarm testing and installation</h1>
           </Link>
+       
+          <Link to="/servies" className="flinks">
+            <h1 className="footerL">Mobile patrol & key holding</h1>
+          </Link>
+          <Link to="/servies" className="flinks">
+            <h1 className="footerL">Construction site security</h1>
+          </Link>
+          
+          <Link to="/services" className="flinks">
+            <h1 className="footerL">Cctv installation</h1>
+          </Link>
+          <Link to="/services" className="flinks">
+            <h1 className="footerL">24/7 Cctv monitoring</h1>
+          </Link>
+          <Link to="/servies" className="flinks">
+            <h1 className="footerL">Manned guarding</h1>
+          </Link>
+          <Link to="/services" className="flinks">
+            <h1 className="footerL">Canine Security</h1>
+          </Link>
+        </div>
+      </div>
+      <div  style={{marginTop:"10px"}}className="bottomLineservices">
+        <div>
+          {" "}
+          <h1 className="footerserviceheading">
+          Our accreditations and certifications to signify our quality of work
+          </h1>
+        </div>
+
+        <div className="certifications">
+          <img src={goals} className="footerLogoservices" />
+          <img
+            src={safe}
+            style={{ marginTop: "10px", marginBottom: "8px" }}
+            className="footerLogoservices"
+          />
+          <img
+            src={ico}
+            style={{ marginTop: "10px", marginBottom: "8px" }}
+            className="footerLogoservices"
+          />
+        </div>
+      </div>
+      <div className="bottomLine">
+        <Link to="/" className="flinks">
+          <img src={logo} className="footerLogo" />
+        </Link>
         <p>
           &copy; {new Date().getFullYear()} SpyconSecurity. All rights reserved.
         </p>
